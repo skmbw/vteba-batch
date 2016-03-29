1f4ef635c4b1b52d97fbf41b9973802eb2a33c2a
skmbw <tonngku2008@126.com>
2016-03-29 20:08:22 +0800

    spring junit test

--- vteba-batch/src/test/java/com/vteba/test/BatchTest.java
+++ vteba-batch/src/test/java/com/vteba/test/BatchTest.java
@@ -1,6 +1,16 @@
 package com.vteba.test;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+@RunWith(value = SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = "classpath:spring/application-context.xml")
 public class BatchTest {
 
+	@Test
+	public void test() {
+		
+	}
 }
