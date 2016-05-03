0dea37de69388d170faedcba22cafb208f260046
skmbw <tonngku2008@126.com>
2016-05-03 13:35:07 +0800

    spring batch 使用3.0.7

---
+++ vteba-batch/src/main/java/com/vteba/batch/user/UserItemWriter.java
@@ -0,0 +1,9 @@
+package com.vteba.batch.user;
+
+import org.springframework.batch.item.adapter.ItemWriterAdapter;
+
+import com.vteba.batch.user.model.User;
+
+public class UserItemWriter extends ItemWriterAdapter<User> {
+
+}
